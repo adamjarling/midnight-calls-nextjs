840d5cae5c46042a80722db3b2dca8da0637f358
Adam J. Arling <aarling@gmail.com>
2023-07-19 12:20:50 -0500

    Update with Forge gig

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../public/images/Midnight Calls Final B&W.png";
 
 const HomeBanner = () => {
   return (
@@ -7,15 +9,10 @@ const HomeBanner = () => {
       style={{ backgroundImage: `url("/images/DSC08971-bw.jpg")` }}
     >
       <div className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full bg-black/40">
-        {/* <div className="relative inline-block max-w-2xl mx-auto animate-pulse">
-          <Image
-            src="/images/midnight-calls-logo_white.png"
-            width="2812"
-            height={2007}
-            alt="Midnight Calls logo"
-          />
-        </div> */}
-        <div className="text-center text-white">
+        <div className="relative inline-block max-w-2xl mx-auto animate-pulse">
+          <Image src={logo} alt="Midnight Calls logo" />
+        </div>
+        {/* <div className="text-center text-white">
           <h1 className="mb-4 text-6xl leading-none tracking-widest md:text-8xl animate-pulse">
             The
             <br />
@@ -24,7 +21,7 @@ const HomeBanner = () => {
           <h2 className="text-3xl tracking-widest opacity-70 md:text-4xl lg:text-5xl">
             Chicago &#9734; USA
           </h2>
-        </div>
+        </div> */}
       </div>
       <p className="absolute text-sm text-right text-white bottom-2 right-5">
         Photo:{" "}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import NYEBanner from "../public/images/Dec31-2022-LONG.jpeg";
 import JasonKane from "../public/images/Feb2023_JasonKane_Poster_PRINT.jpg";
 import forgeFlyer from "../public/images/2023-april-forge.jpeg";
 import forgeFlyerMobile from "../public/images/2023-april-forge-mobile.jpeg";
+import forgeFlyerAug2023 from "../public/images/2023-08-06_forge_1200x1200.jpeg";
 
 export default function Home() {
   return (
@@ -22,84 +23,6 @@ export default function Home() {
       <main>
         <HomeBanner />
 
-        <section>
-          <div className="md:hidden">
-            <Link
-              target="_blank"
-              href="https://www.ticketweb.com/event/yet-again-hazard-the-the-forge-tickets/12878045"
-            >
-              <Image
-                src={forgeFlyerMobile}
-                alt="April 14th at the Forge flyer"
-              />
-            </Link>
-          </div>
-          <div className="hidden md:block">
-            <Link
-              target="_blank"
-              href="https://www.ticketweb.com/event/yet-again-hazard-the-the-forge-tickets/12878045"
-            >
-              <Image src={forgeFlyer} alt="April 14th at the Forge flyer" />
-            </Link>
-          </div>
-        </section>
-
-        <section className="py-0 text-center">
-          <div className="container mx-auto">
-            <Image
-              src={JasonKane}
-              alt="Jason Kane and the Jive residency poster"
-            />
-          </div>
-        </section>
-
-        <section className="">
-          <div className="md:hidden">
-            <Image
-              src="/images/Dec 31 Pipe.jpg"
-              width={825}
-              height={1275}
-              alt="New Years Eve 2022 flyer"
-            />
-          </div>
-          <div className="hidden md:block">
-            <Image src={NYEBanner} alt="New Years Eve 2022 flyer" />
-          </div>
-        </section>
-        <div className="md:hidden">
-          <Link href="https://www.reggieslive.com/show/supersuckers-6/">
-            <Image
-              src="/images/Oct16Supersuckers.jpeg"
-              width={825}
-              height={1275}
-              alt="The Midnight Calls and Supersuckers flyer October 2022"
-            />
-          </Link>
-          <Image
-            src="/images/Jun24TheClaws.jpeg"
-            width={825}
-            height={1275}
-            alt="The Midnight Calls and Supersuckers flyer October 2022"
-          />
-        </div>
-        <div className="hidden md:block">
-          <Link href="https://www.reggieslive.com/show/supersuckers-6/">
-            <Image
-              src="/images/Oct16SupersuckersLONG.jpeg"
-              width={1267}
-              height={599}
-              alt="The Midnight Calls and Supersuckers flyer October 2022"
-              layout="responsive"
-            />
-          </Link>
-          <Image
-            src="/images/Jun24TheClawsLONG.jpeg"
-            width={1267}
-            height={599}
-            alt="The Midnight Calls and Supersuckers flyer October 2022"
-            layout="responsive"
-          />
-        </div>
         <section className="container mx-auto">
           <div className="max-w-xl py-10 mx-auto text-center">
             <p className="pb-5 text-5xl">&#9734;</p>
@@ -109,21 +32,126 @@ export default function Home() {
               Cheap Trick, AC/DC or The Black Crowes. The band is writing
               material for its debut album slated for 2023. Let There Be Rock!
             </p>
-            <p className="pt-10 text-lg">
+            <div className="flex justify-center w-full pt-5 text-2xl">
               <Link href="https://www.facebook.com/themidnightcallsofficial">
-                Follow on Facebook
+                Facebook
               </Link>
-            </p>
-            <p className="text-lg">
-              <Link href="https://www.instagram.com/themidnightcallsofficial/">
-                Get creepy on Instagram
+              <Link
+                href="https://www.instagram.com/themidnightcallsofficial/"
+                className="pl-5"
+              >
+                Instagram
               </Link>
-            </p>
+            </div>
+          </div>
+        </section>
+
+        <section className="container mx-auto">
+          <div className="grid grid-cols-1 gap-10">
+            <div>
+              <Image
+                src={forgeFlyerAug2023}
+                alt="MC at the Forge August 6, 2023"
+                className="mx-auto"
+              />
+              <h2 className="py-5 text-6xl text-center">Next Show!</h2>
+              <div className="flex justify-center w-full pb-10">
+                <Link
+                  href="https://www.ticketweb.com/event/-blacktop-mojo-yet-the-forge-tickets/13266278"
+                  target="_blank"
+                  className="inline-block px-10 py-5 mx-auto text-6xl text-white bg-black hover:text-gray-300"
+                >
+                  Buy Tickets
+                </Link>
+              </div>
+            </div>
+
+            <div className="md:hidden">
+              <Link
+                target="_blank"
+                href="https://www.ticketweb.com/event/yet-again-hazard-the-the-forge-tickets/12878045"
+              >
+                <Image
+                  src={forgeFlyerMobile}
+                  alt="April 14th at the Forge flyer"
+                  className="mx-auto"
+                />
+              </Link>
+            </div>
+
+            <div className="hidden md:block">
+              <Link
+                target="_blank"
+                href="https://www.ticketweb.com/event/yet-again-hazard-the-the-forge-tickets/12878045"
+              >
+                <Image
+                  src={forgeFlyer}
+                  alt="April 14th at the Forge flyer"
+                  className="mx-auto"
+                />
+              </Link>
+            </div>
+
+            <Image
+              src={JasonKane}
+              alt="Jason Kane and the Jive residency poster"
+              className="mx-auto"
+            />
+
+            <Image
+              src="/images/Dec 31 Pipe.jpg"
+              width={825}
+              height={1275}
+              alt="New Years Eve 2022 flyer"
+              className="md:hidden"
+            />
+
+            <Image
+              src={NYEBanner}
+              alt="New Years Eve 2022 flyer"
+              className="hidden mx-auto md:block"
+            />
+
+            <Link href="https://www.reggieslive.com/show/supersuckers-6/">
+              <Image
+                src="/images/Oct16SupersuckersLONG.jpeg"
+                width={1267}
+                height={599}
+                alt="The Midnight Calls and Supersuckers flyer October 2022"
+                className="hidden mx-auto md:block"
+              />
+            </Link>
+
+            <Link href="https://www.reggieslive.com/show/supersuckers-6/">
+              <Image
+                src="/images/Oct16Supersuckers.jpeg"
+                width={825}
+                height={1275}
+                alt="The Midnight Calls and Supersuckers flyer October 2022"
+                className="mx-auto md:hidden"
+              />
+            </Link>
+
+            <Image
+              src="/images/Jun24TheClawsLONG.jpeg"
+              width={1267}
+              height={599}
+              alt="The Midnight Calls and Supersuckers flyer October 2022"
+              className="hidden mx-auto md:block"
+            />
+
+            <Image
+              src="/images/Jun24TheClaws.jpeg"
+              width={825}
+              height={1275}
+              alt="The Midnight Calls and Supersuckers flyer October 2022"
+              className="mx-auto md:hidden"
+            />
           </div>
         </section>
       </main>
 
-      <footer></footer>
+      <footer className="container"></footer>
     </div>
   );
 }
