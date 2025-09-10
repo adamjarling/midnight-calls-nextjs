fe98053136abd04a91d610cbc19e41ad86167178
Adam J. Arling <aarling@gmail.com>
2025-09-10 07:38:17 +0200

    Update all pics wo Tony

--- app/live/HeroWrapper.tsx
+++ app/live/HeroWrapper.tsx
@@ -7,7 +7,7 @@ const HeroWrapper = () => {
   return (
     <div className="relative">
       <ParallaxHero
-        imageUrl={`/photos/DSC08971-bw.jpg`}
+        imageUrl={`/photos/505318457_775036008193220_1042307814692525926_n.jpg`}
         headline="Live"
         altText="MC Live"
         className="h-96 md:h-96"
